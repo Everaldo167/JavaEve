697a4de46b8c40cefac6989ef9d10001c928e29a
Administrador <Administrador@CAML192995>
2021-11-11 20:58:40 -0300

    exemplo WHILE4

---
+++ while/src/pkgwhile/While.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgwhile;
+
+/**
+ *
+ * @author Administrador
+ */
+public class While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int contador = 0;
+            while (contador <=50){
+                System.out.println("Repetição nr:" + contador);
+                contador++;
+            }
+    }
+    
+}
