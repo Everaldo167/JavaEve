02398d71ee0e0a5b4573a25134131966a3908b00
Administrador <Administrador@CAML192995>
2021-11-11 20:56:39 -0300

    exemplowhile2

---
+++ exemplowhile2/src/exemplowhile2/Exemplowhile2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemplowhile2;
+
+public class Exemplowhile2 {
+
+    public static void main(String[] args) {
+    int a = 1;
+    int b = 1;
+    
+        System.out.println("Valor inicial de a = " + a);
+        System.out.println("Valor inicial de b = "+ b);
+        System.out.println("Incrementando: a++");
+        a++;
+        System.out.println("decrementando: b--");
+        b--;
+        System.out.println("Agora a = " + a);
+        System.out.println("Agora b = " + b);
+    }
+    
+}
