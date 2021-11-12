6a80e036a32cea9f62313b18791b26b37108ed00
Administrador <Administrador@CAML192995>
2021-11-11 21:30:16 -0300

    FATORIAL

---
+++ Fatorial/src/fatorial/Fatorial.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fatorial;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Fatorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int fator, resultado=1;
+     
+     for (fator=5; fator>1; fator --)
+         resultado*=fator;
+        System.out.println("Resultado"
+                + "");
+     
+         
+    }
+    
+}
