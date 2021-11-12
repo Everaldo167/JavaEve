6fe549c6252c5bbef7f03df372ea9aea35bca3f2
Administrador <Administrador@CAML192995>
2021-11-11 21:29:28 -0300

    CAD

---
+++ CAD/src/cad/CAD.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cad;
+
+
+public class CAD {
+
+    
+    public static void main(String[] args) {
+     int numeros = 0;
+     for (numeros=1; numeros<=10; numeros++){
+         System.out.println("Tabuada: 1 x " + numeros + " = " + 1* numeros);
+     }
+    }
+    
+}
